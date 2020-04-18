0ec5c2d959c8d295f8c729ac8d9e14a6277b9711
MaximeBolut <60692457+MaximeBolut@users.noreply.github.com>
2020-04-18 14:35:32 +0200

    Update rename_and_shuffle_files.py

--- rename_and_shuffle_files.py
+++ rename_and_shuffle_files.py
@@ -1,31 +1,21 @@
-# Pythono3 code to rename multiple 
-# files in a directory or folder 
+# Pythono3 code to rename multiple files (here png) in a directory or folder 
 # importing os module 
 
 import os 
 import random
-import numpy as np
+
 
 # Function to rename multiple files 
 def main(): 
     #path of the file to be renamed
 	os.chdir("C:\\Users\\images")
 
-	i = 0						  #or 0 if you want your files numbering index to start at 0 
-	mylist =[]
-	j=1
-	itemnuber= len(os.listdir())
-
-	for j in range(itemnuber):
-		mylist.append(str(j))
-		j+=1
-
-
+	mylist =[str(j) for j in range(len(os.listdir()))]
 	random.shuffle(mylist)
-	#print(mylist)
-
+	
+	i = 0
 	for file in os.listdir():
-		src=file                  #old name
+		src=file                           #old name
 		dst="image"+str(mylist[i])+".png"  #new name to be put here (with the type of file)
 		os.rename(src,dst) 
 		i+=1
